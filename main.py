d3a1a782fe5495e1f630ea7ab01a1ec9ac562bd8
Joris Dries <jorisdries@Joriss-MacBook-Pro.local>
2025-06-30 22:41:13 +0200

    Toegevoegd: /health endpoint voor Render

--- main.py
+++ main.py
@@ -34,6 +34,10 @@ def home():
 def ping():
     return "pong", 200
 
+@app.route('/health')
+def health():
+    return "OK", 200
+
 def run():
     port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port, use_reloader=False)
